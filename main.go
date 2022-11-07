e18267a84018094ee6057b344c0d8aae29f586ce
ZILIN YE <yezilin2014@outlook.com>
2022-11-07 15:36:11 -0500

    json test

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	// "encoding/json"
 
-	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"os"
-	"strings"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type RequestInfo struct {
@@ -14,6 +16,13 @@ type RequestInfo struct {
 	request_ID   string
 	student_ID   string
 	Program_code string
+}
+
+type AccessToken struct {
+	access_token string `json:"access_token"`
+	api_domain   string `json:"api_domain"`
+	token_type   string `json:"token_type"`
+	expires_in   int64  `json:"expires_in"`
 }
 
 func main() {
@@ -34,34 +43,46 @@ func main() {
 	// for _, s := range result.Array() {
 	// 	fmt.Println(s)
 	// }
+	RetriveToken()
 
-	readFile, err := os.Open("test.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	var fileLines []RequestInfo
-	for fileScanner.Scan() {
-		downloadslice := strings.Split(fileScanner.Text(), " ")
-		prefix := downloadslice[0]
-		requestID := downloadslice[1]
-		StuID := downloadslice[2]
-		newitem := &RequestInfo{student_ID: StuID, request_ID: requestID, file_prefix: prefix}
-		fileLines = append(fileLines, *newitem)
-	}
+	// readFile, err := os.Open("test.txt")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fileScanner := bufio.NewScanner(readFile)
+	// fileScanner.Split(bufio.ScanLines)
+	// var fileLines []RequestInfo
+	// for fileScanner.Scan() {
+	// 	downloadslice := strings.Split(fileScanner.Text(), " ")
+	// 	prefix := downloadslice[0]
+	// 	requestID := downloadslice[1]
+	// 	StuID := downloadslice[2]
+	// 	newitem := &RequestInfo{student_ID: StuID, request_ID: requestID, file_prefix: prefix}
+	// 	fileLines = append(fileLines, *newitem)
+	// }
 
-	readFile.Close()
+	// readFile.Close()
 
-	for _, line := range fileLines {
-		line.Downloader()
+	// for _, line := range fileLines {
+	// 	line.Downloader()
 
-	}
-
-	// fmt.Println(fileLines)
+	// }
 
 }
 
+func RetriveToken() {
+	var token AccessToken
+	requestBody, err := json.Marshal(token)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, _ := http.Post("https://accounts.zoho.com/oauth/v2/token?refresh_token=1000.23be290456580cd7378b94f2eb3d2334.8ed115d741371a6cf5ada13b2903819e&client_id=1000.6C4D4C3LQS1XV9BVF70PS55G3PELTK&client_secret=211e6b7d3395fd9e8f7d67df464a884e0f573c6079&redirect_uri=https%3A%2F%2Fsign.zoho.com&grant_type=refresh_token", "application/json", bytes.NewBuffer(requestBody))
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(body)
+}
 func (R RequestInfo) Downloader() {
 	fmt.Println(R.file_prefix)
 }
